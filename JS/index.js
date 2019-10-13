d25739e499451f2c52a7f1887de04994f585ed1f
SalamanderJ <joa.bernal@alumnos.duoc.cl>
2019-10-13 18:01:13 -0300

    testeo

--- JS/index.js
+++ JS/index.js
@@ -36,7 +36,20 @@ $(".slides").slidesjs({
     }
       });
 
-
+      $(".accordion-titulo").click(function(){
+		
+        var contenido=$(this).next(".accordion-content");
+           
+        if(contenido.css("display")=="none"){ //open		
+           contenido.slideDown(250);			
+           $(this).addClass("open");
+        }
+        else{ //close		
+           contenido.slideUp(250);
+           $(this).removeClass("open");	
+       }
+                   
+     });
 
  
 
