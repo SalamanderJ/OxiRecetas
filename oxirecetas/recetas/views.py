87f1a3a81a3bd4b1aa24a1b24c01c0cb58632c82
Your Name <bas.moscoso@alumnos.duoc.cl>
2019-11-27 20:29:06 -0300

    Se agrega la navegación entre páginas y se le agrega el css a todas las paginas

--- oxirecetas/recetas/views.py
+++ oxirecetas/recetas/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def index(request):
     return render(request, 'recetas/index.html', {})
 
+def premium(request):
+    return render(request, 'recetas/premium.html', {})
+
+def Comunidad(request):
+    return render(request, 'recetas/Comunidad.html', {})
+
+def registrar(request):
+    return render(request, 'recetas/registrar.html', {})
+
+def vegetariana(request):
+    return render(request, 'recetas/vegetariana.html', {}) 
+
+def italiana(request):
+    return render(request, 'recetas/italiana.html', {})
+
+def chilena(request):
+    return render(request, 'recetas/chilena.html', {})
+
+def francesa(request):
+    return render(request, 'recetas/francesa.html', {})
+
+def comPremium(request):
+    return render(request, 'recetas/comPremium.html', {})
