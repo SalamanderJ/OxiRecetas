cecdc77a6cdcd681286444eb47df6c5469244472
SalamanderJ <joa.bernal@alumnos.duoc.cl>
2019-11-28 17:00:30 -0300

    traspaso de branch

---
+++ oxirecetas/recetas/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('index.html', views.index, name='index'),
+    path('premium.html', views.premium, name='premium'),
+    path('Comunidad.html', views.Comunidad, name='Comunidad'),
+    path('registrar.html', views.registrar, name='registrar'),
+    path('vegetariana.html', views.vegetariana, name='vegetariana'),
+    path('italiana.html', views.italiana, name='italiana'),
+    path('chilena.html', views.chilena, name='chilena'),
+    path('francesa.html', views.francesa, name='francesa'),
+    path('comPremium.html', views.comPremium, name='comPremium'),
+]
