d8a998c1026183368f1675934b7aa9fd1c715c02
Kuzoom <bas.moscoso@alumnos.duoc.cl>
2019-11-29 19:24:12 -0300

    Se eliminan archivos duplicados y se crea la api

--- oxirecetas/oxirecetas/settings.py
+++ oxirecetas/oxirecetas/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'recetas.apps.RecetasConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -99,6 +100,11 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
 
 
 # Internationalization
--- oxirecetas/recetas/urls.py
+++ oxirecetas/recetas/urls.py
@@ -13,3 +13,18 @@ urlpatterns = [
     path('francesa.html', views.francesa, name='francesa'),
     path('comPremium.html', views.comPremium, name='comPremium'),
 ]
+from django.conf.urls import url, include
+from rest_framework import routers
+from recetas.quickstart import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
+
