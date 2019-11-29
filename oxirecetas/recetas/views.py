1730f7000184a74ab36d7fb0bc8ff6e18527b5a9
SalamanderJ <joa.bernal@alumnos.duoc.cl>
2019-11-29 00:07:29 -0300

    eliminarcrud

--- oxirecetas/recetas/views.py
+++ oxirecetas/recetas/views.py
@@ -50,6 +50,11 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'recetas/post_edit.html', {'form': form})
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('Comunidad')
     
 
 def registrar(request):
