c3a3b2cc742724666ea180ef86f404bc137b6432
Kuzoom <bas.moscoso@alumnos.duoc.cl>
2019-11-30 02:46:51 -0300

    se hace un cambio en urls.py

--- oxirecetas/recetas/urls.py
+++ oxirecetas/recetas/urls.py
@@ -1,3 +1,18 @@
+
+from django.conf.urls import url, include
+from rest_framework import routers
+from recetas.quickstart import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
 from django.urls import path
 from . import views
 
@@ -12,19 +27,5 @@ urlpatterns = [
     path('chilena.html', views.chilena, name='chilena'),
     path('francesa.html', views.francesa, name='francesa'),
     path('comPremium.html', views.comPremium, name='comPremium'),
-]
-from django.conf.urls import url, include
-from rest_framework import routers
-from recetas.quickstart import views
-
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
-urlpatterns = [
-    url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
 
