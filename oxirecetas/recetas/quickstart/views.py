72cdbb528d0cb72ca6f55ab35310c0c02c6c6fc5
Kuzoom <bas.moscoso@alumnos.duoc.cl>
2019-11-30 02:45:49 -0300

    Se arregla otro error en views.py

--- oxirecetas/recetas/quickstart/views.py
+++ oxirecetas/recetas/quickstart/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from oxirecetas.recetas.quickstart.serializers import UserSerializer, GroupSerializer
+from recetas.quickstart.serializers import UserSerializer, GroupSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     """
